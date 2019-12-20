7dcb76de777a5038c17348ec87709fbaeb58cd57
aaronjan <aaronjan98@gmail.com>
2019-12-20 08:29:42 -0800

    added the api, but still not working...

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,12 +1,33 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    axios
+      .get(
+        `https://rickandmortyapi.com/api/character/`,
+      )
+      .then(response => {
+        const characters = response.results.filter(
+          character =>
+            character.name
+              .toLowerCase()
+              .includes(query.toLowerCase())
+        );
+         console.log("Rick & Morty characters", response);
+        setData(characters);
+      });
+  }, [query]);
+
+  const handleInputChange = event => {
+    setQuery(event.target.value);
+  };
 
   return (
     <section className="character-list">
